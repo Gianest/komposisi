60adcb0f5bc23d67e7165e5b640812f99e6cb910
dsgfresg <dsgdasf@>
2019-07-02 12:12:28 +0700

    Initial commit

---
+++ Mobil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace komposisi
+{
+    public class Mobil
+    {
+        public string Merk { get; set; }
+        private Mesin mesin;
+        public Mobil()
+        {
+            mesin = new Mesin();
+            mesin.Type = "INLINE";
+        }
+        public Mesin GetlnfoMesin()
+        {
+            return mesin;
+        }
+
+        public class Mesin 
+        { public string Type { get; set; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace komposisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Demo Relasi Komposisi";
+            Mobil mobil = new Mobil();
+            mobil.Merk = "BMW";
+            Console.WriteLine("Merek mobil: {0}, Tipe mesin: {1}",
+            mobil.Merk, mobil.GetlnfoMesin().Type);
+            Console.ReadKey();
+        }
+    }
+}
